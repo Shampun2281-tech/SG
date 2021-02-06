85864d2d45ee2e0803758a0de646e3ca5ed4a28b
Shampun2281-tech <70425919+Shampun2281-tech@users.noreply.github.com>
2021-02-06 16:53:46 +0300

    Заготовка №1
    Первые перменные и минюшка!

--- SG.py
+++ SG.py
@@ -4,4 +4,12 @@ from playsound import playsound
 name = "	Soviet Guy"
 version = "v1.0"
 def menu():
-	
+	os.system('cls||clear')
+	print(" " + name + " " + (version) + "		Made by Shampun2281" )
+	print("")
+	print("Миссии (В разработке)")
+	print("Настройки (В разработке)")
+	print("Статистика (В разработке)")
+	print("")
+	zapros = input("Ввод:	")
+menu()
