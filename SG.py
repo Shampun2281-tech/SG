5394fcb280b23c306c4d0e3ef9ea4cce7de0e6f0
Shampun2281-tech <70425919+Shampun2281-tech@users.noreply.github.com>
2021-02-06 17:11:03 +0300

    Ещё больше меню и буковок!

--- SG.py
+++ SG.py
@@ -7,9 +7,48 @@ def menu():
 	os.system('cls||clear')
 	print(" " + name + " " + (version) + "		Made by Shampun2281" )
 	print("")
-	print("Миссии (В разработке)")
-	print("Настройки (В разработке)")
-	print("Статистика (В разработке)")
+	print("		1 - Миссии (В разработке)")
+	print("		2 - Настройки (В разработке)")
+	print("		3 - Статистика (В разработке)")
 	print("")
-	zapros = input("Ввод:	")
+	zapros = input("	Ввод:	")
+	if zapros == "1":
+		missions()
+	if zapros == "2":
+		settings()
+	if zapros == "3":
+		stats()
+	menu()
+def missions():
+	os.system('cls||clear')
+	print(" " + name + " " + (version) + "		Made by Shampun2281" )
+	print("")
+	print("		0 - Выход")
+	print("		1 - Учебка")
+	print("")
+	zapros = input("	Ввод:	")
+	if zapros == "0":
+		menu()
+	missions()
+def settings():
+	os.system('cls||clear')
+	print(" " + name + " " + (version) + "		Made by Shampun2281" )
+	print("")
+	print("		0 - Выход")
+	print("		1 - Персонаж (В разработке)")
+	print("")
+	zapros = input("	Ввод:	")
+	if zapros == "0":
+		menu()
+	settings()
+def stats():
+	os.system('cls||clear')
+	print(" " + name + " " + (version) + "		Made by Shampun2281" )
+	print("")
+	print("		0 - Выход")
+	print("")
+	zapros = input("	Ввод:	")
+	if zapros == "0":
+		menu()
+	stats()
 menu()
